4f1b66a9b8b7c43c6e73a329ec3a808c3e8e3134
TGHDataSudan <45471236+TGHDataSudan@users.noreply.github.com>
2018-12-28 00:42:48 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -125,7 +125,7 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon});
-        marker.bindTooltip(point['Name'],{ permanent: true,direction: 'right'});
+        marker.bindTooltip(point['Name'],{ permanent: false , direction: 'right'});
       
         var AddCircle = point['CRadius'] !=='';
         
