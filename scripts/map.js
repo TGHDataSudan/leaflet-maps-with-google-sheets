e304fad181be3d897792388820f1a17daaa014b4
TGHDataSudan <45471236+TGHDataSudan@users.noreply.github.com>
2018-12-27 16:20:21 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -126,7 +126,7 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
 //serge        var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-        var marker = L.circle([point.Latitude, point.Longitude], {  color: point['Icon Color'],  fillColor: '#f03',    fillOpacity: 0.5,    radius: point.Size})
+        var marker = L.circle([point.Latitude, point.Longitude], {  color: point['Icon Color'],  fillColor: point['Icon Color'],    fillOpacity: 0.5,    radius: point.Size})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
           point['Description']);
