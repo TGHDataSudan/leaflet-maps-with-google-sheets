14caf38196a4b78c912b208d59ac489b3c0eb370
TGHDataSudan <45471236+TGHDataSudan@users.noreply.github.com>
2018-12-30 16:59:45 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -131,8 +131,8 @@ $(window).on('load', function() {
         
         if (AddCircle) {
           //var Cmarker = L.circleMarker([point.Latitude, point.Longitude], {color: point['Marker Color'], weight: 1, fillColor: point['Marker Color'], fillOpacity: 0.8, radius: point['CRadius']}) ;    
-          //var Cmarker = L.RegularPolygonMarker([point.Latitude, point.Longitude], {numberOfSides: 5, color: point['Marker Color'], fillOpacity: 0.8, radius: point['CRadius']}) ; 
-          var Cmarker = new L.RegularPolygonMarker([point.Latitude, point.Longitude], {numberOfSides: 3, rotation: 60.0,	radius: 10 });
+          var Cmarker = new L.RegularPolygonMarker([point.Latitude, point.Longitude], {numberOfSides: 5, color: point['Marker Color'], fillOpacity: 0.8, radius: point['CRadius']}) ; 
+          //var Cmarker = new L.RegularPolygonMarker([point.Latitude, point.Longitude], {numberOfSides: 3, rotation: 60.0,	radius: 10 });
         }
           
         marker.bindPopup("<b>" + point['Name'] + '</b><br>' +
