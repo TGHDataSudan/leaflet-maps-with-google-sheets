b2730a169f09c5ea4ed0705d950863a095a37bce
TGHDataSudan <45471236+TGHDataSudan@users.noreply.github.com>
2018-12-30 22:51:19 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -111,7 +111,6 @@ $(window).on('load', function() {
                                                     fillOpacity: 0.8, 
                                                     imageCircleUrl: point['Marker Icon'], 
                                                     radius: point['Radius'],
-                                                    innerRadius: point['InnerRadius'],
                                                    }) ; 
         } else {
           // If icon contains '.', assume it's a path to a custom icon,
